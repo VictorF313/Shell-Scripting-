3a88fa1e22ccc807641a455ba2f9d8140bafe3ef
VictorF313 <129792251+VictorF313@users.noreply.github.com>
2023-04-04 15:24:32 +0530

    Update setup.sh

--- YouTube Downloader/setup.sh
+++ YouTube Downloader/setup.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
-pkg update
-pkg upgrade -y
+echo -e "Downloading the dependencies"
+pkg update && pkg upgrade -y
 pkg install python
 pkg install python-pip
 pip install yt-dlp
+echo -e "Give access to the storage"
+sleep 2
+termux-setup-storage
+sleep 5
