b34226a97a46a8aa0cbab58a437584ef28deb8f5
VictorF313 <129792251+VictorF313@users.noreply.github.com>
2023-06-04 03:55:00 +0530

    Update setup.sh

--- YouTube Downloader/setup.sh
+++ YouTube Downloader/setup.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/bash
+mkdir ~bin
+cd bin/
+mkdir termux-url-opener
+chmod +x termux-url-opener
+sleep 5
+cd
 echo -e "Downloading dependencies"
 pkg update && pkg upgrade -y
 pkg install python
